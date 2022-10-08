68fb64b3a64b8154c510ad060a3c8b938c38bcdc
p-s-vishnu <psvishnu.it13@gmail.com>
2022-10-08 12:32:19 +0100

    add supporting assets

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@ $('.responsive').on('click', function (e) {
     
     // HOME TYPED JS
       $(".element").typed({
-        strings: ["I'm Vishnu", "Data Practitioner","Product Engineer"],
+        strings: ["I'm Vishnu", "Data Scientist", "AI/ML Engineer"],
         typeSpeed: 10,
         loop:true,
         backDelay: 2000
