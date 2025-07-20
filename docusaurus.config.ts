5cca700448620240c963502c03812f1607ea72ea
p-s-vishnu <psvishnu.it13@gmail.com>
2025-07-20 02:04:17 +0100

    baseurl update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://p-s-vishnu.github.io', // Updated with your GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/p-s-vishnu.github.io/', // Updated with your repository name
+  baseUrl: '/', // Set to '/' for custom domain deployment on GitHub Pages
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
